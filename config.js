eea249ce1854f42ccac01664248ac954addd8837
eothe9s <hahahajeong92@gmail.com>
2026-09-11 19:33:54 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+// 공개 키만 들어 있습니다. 비밀번호나 secret 키를 넣지 마세요.
 window.TRAVEL_CONFIG = {
   SUPABASE_URL: "https://glbmuxdbjgbgmhbbmnai.supabase.co",
   PUBLIC_KEY: "sb_publishable_G3NuSE5ACtFOyXjmy3QrDw_yYcgSgV-"
